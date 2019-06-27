9f52c397e2c1a377682322367ae5be777e7e671f
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2019-06-27 23:30:40 +0200

    Improve specs for runner

--- jestsetup.js
+++ jestsetup.js
@@ -1,6 +1,8 @@
 const TR_BASE_URL = 'https://traduora.example.com/some-path/'
+const TR_PROJECT_ID = 'xyz'
 
 process.env = {
   ...process.env,
   TR_BASE_URL,
+  TR_PROJECT_ID,
 }
--- src/runner/index.spec.js
+++ src/runner/index.spec.js
@@ -1,7 +1,29 @@
-import runner from './index'
+jest.mock('../modules/getAuthToken', () => jest.fn(() => 'JWT'))
+jest.mock('../modules/axiosInstance', () => ({
+  setAuthToken: jest.fn(() => {}),
+  setBaseUrl: jest.fn(() => {}),
+}))
 
 describe('runner', () => {
-  it('should exist', () => {
-    expect(runner).toBeDefined()
+  beforeEach(() => {
+    jest.resetModules()
+    jest.doMock('../tasks', jest.fn(() => ({ myTask: jest.fn(() => {}) })))
+  })
+
+  it('should invoke the desired task', async () => {
+    const taskMock = require('../tasks').myTask
+    const { setBaseUrl, setAuthToken } = require('../modules/axiosInstance')
+    const runner = require('./index').default
+
+    await runner('myTask', '/foo')
+
+    expect(setAuthToken.mock.calls[0][0]).toBe('JWT')
+    expect(setBaseUrl.mock.calls[0][0]).toBe('https://traduora.example.com/some-path/projects/xyz/')
+    expect(taskMock.mock.calls[0][0]).toEqual(
+      expect.objectContaining({
+        'client-id': 'test',
+        'pull-to': '/abs-path/foo/intl/pull/<locale>.<hash:6>.json',
+      }),
+    )
   })
 })
