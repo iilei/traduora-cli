668bd340a4994a2ca2121fb57b1e996f7ba2f1da
CyrilLD <c.laguilhon.debat@petitclou.de>
2019-07-29 09:54:45 +0200

    Feat (getExport) : Add an option to remove empty lines from exports

--- src/modules/getExport.js
+++ src/modules/getExport.js
@@ -1,6 +1,22 @@
 import { axiosGet } from './axiosInstance'
+import config from './config'
+
+const { 'drop-empty': dropEmpty = false } = config
+
 
 const getExport = async (locale, format) =>
-  axiosGet(`/exports?locale=${locale}&format=${format}`).then(result => ({ [locale]: result }))
+  axiosGet(`/exports?locale=${locale}&format=${format}`)
+  .then(result => {
+
+    if (dropEmpty === true) {
+      for (const key in result) {
+        if (result[key] === '') {
+          delete result[key]
+        }
+      }
+    }
+
+    return ({ [locale]: result })
+  })
 
 export default getExport
