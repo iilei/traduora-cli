55241a99f63365bad656aac0a8db0a07e921e7b0
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2019-12-31 12:13:37 +0100

    Minor: error handling

--- src/modules/getHealth.js
+++ src/modules/getHealth.js
@@ -1,16 +1,18 @@
 import axios from './axiosInstance'
 
-const introspect = ({ code }) => {
+const introspect = error => {
   const known = {
     ECONNREFUSED: `Can not reach "${axios.defaults.baseURL}/health"!`,
   }
 
-  return known[code] || 'EHEALTHCHECK'
+  return known[error.code] || JSON.stringify(error)
 }
 
 const getHealth = async () => {
   return axios
-    .get('/health')
+    .get('/health', {
+      validateStatus: status => status < 300,
+    })
     .then(response => response.data)
     .catch(error => {
       console.log(introspect(error))
